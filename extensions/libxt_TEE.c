1fd1787a1c824ddb968d29ac97f280f17c028c33
Jan Engelhardt <jengelh@medozas.de>
2009-01-10 09:57:44 +0100

    TEE: limit iptables module to NFPROTO_IPV4
    The code here is only usable with IPv4.

--- extensions/libxt_TEE.c
+++ extensions/libxt_TEE.c
@@ -98,6 +98,8 @@ static void tee_tg_save(const void *ip, const struct xt_entry_target *target)
 static struct xtables_target tee_tg_reg = {
 	.name          = "TEE",
 	.version       = XTABLES_VERSION,
+	.revision      = 0,
+	.family        = PF_INET,
 	.size          = XT_ALIGN(sizeof(struct xt_tee_tginfo)),
 	.userspacesize = XT_ALIGN(sizeof(struct xt_tee_tginfo)),
 	.help          = tee_tg_help,
