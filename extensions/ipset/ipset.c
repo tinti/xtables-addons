0de9f38412331231f66c7104f56065ef7c495908
Jan Engelhardt <jengelh@medozas.de>
2010-01-25 11:54:34 +0100

    ipset: update to 4.2

--- extensions/ipset/ipset.c
+++ extensions/ipset/ipset.c
@@ -30,7 +30,7 @@
 #define PROC_SYS_MODPROBE "/proc/sys/kernel/modprobe"
 #endif
 
-#define IPSET_VERSION "4.1"
+#define IPSET_VERSION "4.2"
 
 char program_name[] = "ipset";
 char program_version[] = IPSET_VERSION;
